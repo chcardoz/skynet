1e097fe8cb6d26fe6e58268c61e008b0e35d40e8
Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
2025-07-28 21:57:59 -0700

    Creates a simple guessing game with rand library and uses fmt for formatting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::rng().random_range(1..=100);
+    let mut num_guesses = 0;
+
+    loop {
+        println!("Guess the number!");
+        let mut num = String::new();
+        io::stdin()
+            .read_line(&mut num)
+            .expect("Failed to read line");
+        let num: u32 = num.trim().parse::<u32>().expect("Please type a number!");
+        num_guesses += 1;
+        if num < secret_number {
+            println!("Too small!");
+        } else if num > secret_number {
+            println!("Too big!");
+        } else {
+            println!("You win!");
+            println!("You guessed {num_guesses} times");
+            break;
+        }
+    }
 }
